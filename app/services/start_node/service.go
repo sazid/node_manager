4400bfe0768539852c9e8a7f0171ed65b1475e94
Mohammed Sazid Al Rashid <sazidozon@gmail.com>
2021-06-17 02:20:31 +0600

    add `start node` service

--- app/services/start_node/service.go
+++ app/services/start_node/service.go
@@ -2,15 +2,53 @@ package start_node
 
 import (
 	"context"
+	"log"
+	"node_manager/app"
+	"node_manager/app/services/bootstrap_node"
 	"node_manager/app/store"
+	"os/exec"
+	"path/filepath"
 )
+
+const nodeCliFile = "node_cli.py"
 
 // Service starts a node and leaves it running. A node goes into the
 // `Idle` state when it first starts and go online.
 type Service struct {
-	config store.Config
+	config           store.Config
+	bootstrapNodeSrv app.Service
 }
 
-func (s *Service) Run(context.Context, interface{}) (result interface{}, err error) {
-	return nil, nil
+type Result struct {
+	Path string
+}
+
+func (s *Service) Run(ctx context.Context, _ interface{}) (result interface{}, err error) {
+	r, err := s.bootstrapNodeSrv.Run(ctx, nil)
+	result = Result{
+		Path: r.(bootstrap_node.Result).Path,
+	}
+	log.Println(result.(Result).Path)
+	if err != nil {
+		return
+	}
+
+	nodeCliPath := filepath.Join(result.(Result).Path, nodeCliFile)
+
+	var nodeCliArgs = []string{
+		nodeCliPath,
+		"-s",
+		"https://github.com",
+		"-k",
+		"123ABC-456DEF-789GHI-101JKL",
+		"--once",
+	}
+
+	cmd := exec.CommandContext(ctx, "python3", nodeCliArgs...)
+	err = cmd.Start()
+	if err != nil {
+		return
+	}
+
+	return
 }
--- app/services/start_node/service_test.go
+++ app/services/start_node/service_test.go
@@ -2,16 +2,34 @@ package start_node
 
 import (
 	"context"
+	"node_manager/app"
+	"node_manager/app/services/bootstrap_node"
+	"node_manager/app/test_utils"
+	"os"
 	"testing"
 )
 
 func TestStartNode(t *testing.T) {
 	ctx := context.Background()
-	srv := Service{}
 
-	_, err := srv.Run(ctx, nil)
+	srv := Service{
+		bootstrapNodeSrv: stubBootstrapNodeSrv(t),
+	}
+
+	res, err := srv.Run(ctx, nil)
+	defer os.RemoveAll(res.(Result).Path)
 
 	if err != nil {
 		t.Errorf("did not expect an error, got %+v, want %+v", err, nil)
+	}
+}
+
+func stubBootstrapNodeSrv(t testing.TB) app.ServiceFunc {
+	return func(context.Context, interface{}) (result interface{}, err error) {
+		nodeDir := test_utils.GenerateTempNode(t, nil)
+
+		return bootstrap_node.Result{
+			Path: nodeDir,
+		}, nil
 	}
 }
